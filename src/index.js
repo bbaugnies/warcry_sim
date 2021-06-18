17d32f5c42354c3696a1deb0d7013be2651b7256
BENJAMIN-PC\Benjamin <bbaugnies@gmail.com>
2021-06-18 16:06:30 +0200

    sim should work

--- src/index.js
+++ src/index.js
@@ -203,10 +203,57 @@ function attachListeners() {
     userInputScreen.focus();
   }
 
-
+    function factorialize(num) {
+      if (num < 0) 
+            return -1;
+      else if (num == 0) 
+          return 1;
+      else {
+          return (num * factorialize(num - 1));
+      }
+    }
+    
+    function ncr(n, r) {
+        r = Math.min(r, n-r);
+        var numerator = factorialize(n);
+        var denominator = factorialize(r) * factorialize(n-r);
+        return numerator/denominator;
+    }
+    
     function sim () {
-        let strength = document.getElementById("strength").value;
-        document.getElementById("output").value = "current strength is: " + strength
+        var strength = document.getElementById("strength").value;
+        var attacks = document.getElementById("attacks").value;
+        var toughness = document.getElementById("toughness").value;
+        var hitpoints = document.getElementById("hitpoints").value;
+        var damage = document.getElementById("damage").value;
+        var crit_damage = document.getElementById("crit_damage").value;
+        var s_attacks = document.getElementById("s_attacks").value;
+        var s_hit = document.getElementById("s_hit").value;
+        var s_crit = document.getElementById("s_crit").value;
+        var s_damage = document.getElementById("s_damage").value;
+        var s_crit_damage = document.getElementById("s_crit_damage").value;
+        
+        var kills = 0;
+        
+        for (var i = 0; i < attacks; i++){
+            for (var j = 0; j < s_attacks; j++) {
+                for (var k = 0; k < attacks - i; k++) {
+                    for (var l = 0; l < s_attacks - j; l ++){
+                        if (i*crit_damage + j*s_crit_damage + k*damage + l*s_damage >= hitpoints) {
+                            var n = ncr(attacks, i);
+                            n = n * ncr(s_attacks, j) * ((7-s_crit)**j);
+                            n = n * ncr(attacks - i, k) * ((6 - Math.max(3, Math.min(5, 4 + toughness - strength))) ** k);
+                            n = n * nrc(s_attacks - j, l) * ((s_crit - s_hit) ** l);
+                            n = n * ((6 - Math.max(3, Math.min(5, 4 + toughness - strength))) ** (attacks - i -k));
+                            n = n * ((6 - (7-s_crit) - (s_crit - s_hit)) ** (s_attacks-j-l));
+                            kills += n
+                        }
+                    }
+                }
+            }
+        }
+        
+        document.getElementById("output").value = kills/(6**(attacks+s_attacks))*100;
     }
     
     document.querySelector('.simBtn')
