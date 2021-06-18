e67a95f24559c2958821d0bf4cb0446ad601fc43
BENJAMIN-PC\Benjamin <bbaugnies@gmail.com>
2021-06-18 16:50:11 +0200

    no message

--- src/index.js
+++ src/index.js
@@ -260,8 +260,8 @@ function attachListeners() {
                 }
             }
         }
-        console.log(kills);
-        console.log(attacks);
+        console.log("kills:" + kills);
+        console.log("attacks:" + 6**(attacks+s_attacks));
         document.getElementById("output").value = (kills/(6**(attacks+s_attacks)))*100;
     }
     
